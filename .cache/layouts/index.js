be3f282097393615ffd6ab5f3df28e967888ad92
Tyler Stephen Maschino <tylermaschino@gmail.com>
2018-08-11 21:08:38 -0400

    Removed boilerplate styles

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "C:/Users/Tyler/Documents/tylerstephenmaschino/src/layouts/index.js"
+  import data from "C:\\Users\\Tyler\\Documents\\tylerstephenmaschino\\.cache\\json\\layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,37 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div id="hero">
+      <h1>Tyler Stephen Maschino</h1>
+      <h2>Engineer. Inventor. Entrepreneur.</h2>
+      <Link to="#about-me" class="navButton">About Me</Link>
+      <Link to="#contact"class="navButton">Contact</Link>
+      <Link to="/portfolio/"class="navButton">Portfolio</Link>
+      <Link to="/blog/"class="navButton">Blog</Link>
+    </div>
+    <div id="about-me">
+      <div>About Me</div>
+      <div id="interests">
+        <div>Engineering</div>
+        <div>Coffee</div>
+        <div>Reading</div>
+        <div>Entrepreneurship</div>
+      </div>
+    </div>
+    <div id="contact">
+      <h2>Hit me up.</h2>
+      <div id="contactInfo">
+      <ul>
+        <li>Facebook</li>
+        <li>Twitter</li>
+        <li>GitHub</li>
+        <li>LinkedIn</li>
+        <li>Email</li>
+      </ul>
+      </div>
+    </div>
+    <div id="contact"></div>
+    <div id="footer">I am a footer.</div>
   </div>
 )
 
