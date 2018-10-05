96c3c9f60602075fdc66b1a0cd5589bb35fc3dd3
Tyler Maschino <Tylermaschino@gmail.com>
2018-10-05 17:34:06 -0400

    Added website title and updated favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,21 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Default Starter'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Tyler Stephen Maschino',
+        short_name: 'TSM',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
+        icon: 'src/images/tsm_logo.png' // This path is relative to the root of the site.
+      }
     },
-    'gatsby-plugin-offline',
-  ],
+    'gatsby-plugin-offline'
+  ]
 }
