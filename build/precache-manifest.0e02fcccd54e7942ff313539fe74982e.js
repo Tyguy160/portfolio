040fa62a4ed78ee382c77e3334627c7bc1f88992
Tyler Maschino <Tylermaschino@gmail.com>
2018-10-09 05:56:10 -0400

    Added redirects file and build folder

---
+++ build/precache-manifest.0e02fcccd54e7942ff313539fe74982e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a615b96239c3deb2f3870e2a9df767f5",
+    "url": "/static/media/tsm_logo.a615b962.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "60ac45409d47d066cac34b6ddc9880fc",
+    "url": "/static/media/hero.60ac4540.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af2f6a229e44859663cc",
+    "url": "/static/js/main.af2f6a22.chunk.js"
+  },
+  {
+    "revision": "b5a4f037b46ad1d8b834",
+    "url": "/static/js/1.b5a4f037.chunk.js"
+  },
+  {
+    "revision": "af2f6a229e44859663cc",
+    "url": "/static/css/main.8935fee1.chunk.css"
+  },
+  {
+    "revision": "70e7dc8851761fef446ccbdedd1dfa66",
+    "url": "/index.html"
+  }
+];
