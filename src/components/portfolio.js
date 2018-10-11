a18da74fa226f44fdb4264e0e44b65656777086b
Tyler Maschino <Tylermaschino@gmail.com>
2018-10-11 19:48:05 -0400

    Added pieces to the portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,12 +1,47 @@
 import React from "react";
 
 import Header from "./header";
+import PortfolioPiece from "./portfolioPiece";
 
-const PortfolioPage = () => (
-  <div>
-    <Header />
-    <h2>My Endeavors</h2>
-  </div>
-);
+import "../styles/portfolio.css";
+
+// Thumbnail images
+import smoothistaImage from "../images/smoothista.png";
+import brewTimeImage from "../images/brew-time.png";
+import shiftListImage from "../images/shift-list.png";
+
+const PortfolioPage = () => {
+  // Portfolio URLs
+  let smoothistaURL = "https://smoothista.com";
+  let brewTimeURL = "https://triplebarcoffee.com/brew-time";
+  let shiftListURL = "https://shiftlist.netlify.com";
+
+  return (
+    <div className="portfolio-page">
+      <Header />
+      <h2>My Endeavors</h2>
+      <div className="portfolio">
+        <PortfolioPiece
+          className="portfolio-piece"
+          title={"Smoothista"}
+          imgSrc={smoothistaImage}
+          url={smoothistaURL}
+        />
+        <PortfolioPiece
+          className="portfolio-piece"
+          title={"Shift List"}
+          imgSrc={shiftListImage}
+          url={shiftListURL}
+        />
+        <PortfolioPiece
+          className="portfolio-piece"
+          title={"Brew Time"}
+          imgSrc={brewTimeImage}
+          url={brewTimeURL}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default PortfolioPage;
---
+++ src/components/portfolioPiece.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/portfolioPiece.css";
+
+const PortfolioPiece = props => {
+  return (
+    <div>
+      <div className="thumbnail-container">
+        <a href={props.url} alt={props.title}>
+          <div className="thumbnail">
+            <img src={props.imgSrc} title={props.title} frameBorder="0" />
+          </div>
+        </a>
+      </div>
+      <a href={props.url} alt={props.title} class="portfolio-link" style={{}}>
+        <h3 className="description">{props.title}</h3>
+      </a>
+    </div>
+  );
+};
+
+export default PortfolioPiece;
